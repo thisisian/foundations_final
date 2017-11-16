4b3a70f0806222372d3dbf56118183ef81c4cdb1
GavinMegson <33714341+GavinMegson@users.noreply.github.com>
2017-11-16 13:10:13 -0800

    Update functions.h

--- functions.h
+++ functions.h
@@ -32,3 +32,4 @@ void getdirname(int dir, char s[]);
 stack_element *getdirs(char start[], char end[], node *junc);
 node * findnode(char s[], node *junc, int *dir);
 void inputformat(char s[]);
+void popall(stack_element * stack, char end[]);
